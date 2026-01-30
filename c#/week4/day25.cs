b95c6e6a32a1403d69421fb956588b476d2afebd
gue02 <kyu6716@naver.com>
2026-01-30 23:19:00 +0900

    day25 , add c#

---
+++ c#/week4/day25.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int age = 25;
+        string name = "민수";
+        double height = 180.2;
+        bool student = true;
+
+        Console.WriteLine($"이름: {name}");
+        Console.WriteLine($"나이: {age}");
+        Console.WriteLine($"키: {height}");
+        Console.WriteLine($"학생 여부: {student}");
+    }
+}
