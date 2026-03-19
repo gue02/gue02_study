da2bcc8159fecbeb10a7e51f697712e36ae422ad
gue02 <kyu6716@naver.com>
2026-03-20 00:10:30 +0900

    day38

---
+++ c++/week6/day38/day38.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+	int data;
+	Node* left;
+	Node* right;
+	
+	Node(int value) {
+		data = value;
+		left = nullptr;
+		right = nullptr;
+	}
+};
+
+Node* insert(Node* root, int value) {
+	if (root == nullptr)
+		return new Node(value);
+	
+	if(value < root->data)
+		root->left = insert(root->left, value);
+	else if (value > root)
+		root->right = insert(root->right, value);
+		
+	return root;
+}
+
+Node* findMin(Node* root){
+	while (root && root->left != nullptr){
+		root = root->left;
+	}
+	return root;
+}
+
+Node* deleteNode(Node* root. int value){
+	if (root == nullptr)
+		return nullptr;
+	
+	if (value < root->data) {
+		root->left = deleteNode(root->left, value);
+	}
+	else if (value > root->data) {
+		root->right = deleteNode(root->right, value);
+	}
+	else {
+		if(root->left == nullptr && root -> right == nullptr) {
+		delete root;
+		return nullptr;
+		}
+		else if (root->left == nullptr){
+			Node* temp = root -> left;
+			delete root;
+			return temp;
+		}
+		else {
+			Node* successor = findMin(root->right);
+			root->data = successor->data;
+			root->right = deleteNode(root->right, successor->data);
+		}
+	}
+	return root;
+}
+
+void inorder(Node* root) {
+	if (root == nullptr) return;
+	inorder(root->left);
+	cout << root->data << " ";
+	inorder(root->right);
+}
+int main(){
+	Node* root = nullptr;
+	
+	root = insert(root, 50);
+	root = insert(root, 30);  
+	root = insert(root, 70);  
+	root = insert(root, 20);  
+	root = insert(root, 40);  
+	root = insert(root, 60);  
+	root = insert(root, 80);  
+	  
+	cout << "삭제 전: ";  
+	inorder(root);  
+	cout << endl;  
+	  
+	root = deleteNode(root, 50);  
+	  
+	cout << "50 삭제 후: ";  
+	inorder(root);  
+	cout << endl;  
+	  
+	return 0;
+}
